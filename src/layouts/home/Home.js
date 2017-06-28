230c4edf3a555788ca2107be673c53c4d1473ff4
Kevin Seagraves <k.s.seagraves@gmail.com>
2017-06-28 16:31:03 -0600

    setup environment

---
+++ src/layouts/home/Home.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <main className="container" id="wrapper">
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <div className="App">
+
+
+                <footer className="footer">
+                <div className="footerContent">
+                <p>Curated events for a personalized calendar</p>
+                <small >&copy; 2017 eventtarget.com ALL RIGHTS RESERVED</small>
+                </div>
+                </footer>
+
+
+            </div>
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Home
